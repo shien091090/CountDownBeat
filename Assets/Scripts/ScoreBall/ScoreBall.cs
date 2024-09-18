825e5a7e9b2a7f3da0342a0e8faa5745d55e031f
NK.Shien <lithoshu@yile.com.tw>
2024-09-18 12:47:23 +0800

    分數球  1.串接Beat事件, 收到事件時倒數數字減一  2.Add Test Case*2
    a.設定初始化時, 監聽事件
    b.收到Beat事件時, 倒數數字減一

--- Assets/Scripts/ScoreBall/ScoreBall.cs
+++ Assets/Scripts/ScoreBall/ScoreBall.cs
@@ -1,10 +1,19 @@
+using SNShien.Common.ProcessTools;
+
 namespace GameCore
 {
     public class ScoreBall
     {
+        private readonly IEventRegister eventRegister;
+
         public int StartCountDownValue { get; private set; }
         public int CurrentCountDownValue { get; private set; }
         public ScoreBallState CurrentState { get; private set; }
+
+        public ScoreBall(IEventRegister eventRegister)
+        {
+            this.eventRegister = eventRegister;
+        }
 
         public void Init(int startCountDownValue)
         {
@@ -14,6 +23,19 @@ namespace GameCore
             StartCountDownValue = startCountDownValue;
             CurrentCountDownValue = StartCountDownValue;
             CurrentState = ScoreBallState.InCountDown;
+
+            RegisterEvent();
+        }
+
+        private void RegisterEvent()
+        {
+            eventRegister.Unregister<BeatEvent>(OnBeat);
+            eventRegister.Register<BeatEvent>(OnBeat);
+        }
+
+        private void OnBeat(BeatEvent eventInfo)
+        {
+            CurrentCountDownValue--;
         }
     }
 }
--- Assets/Scripts/UnitTests/Editor/ScoreBallTest.cs
+++ Assets/Scripts/UnitTests/Editor/ScoreBallTest.cs
@@ -1,15 +1,22 @@
+using System;
+using NSubstitute;
 using NUnit.Framework;
+using SNShien.Common.ProcessTools;
 
 namespace GameCore.UnitTests
 {
     public class ScoreBallTest
     {
-        private static ScoreBall scoreBall;
+        private ScoreBall scoreBall;
+        private IEventRegister eventRegister;
+        private Action<BeatEvent> beatEventCallback;
 
         [SetUp]
-        public static void Setup()
+        public void Setup()
         {
-            scoreBall = new();
+            InitEventHandlerMock();
+
+            scoreBall = new(eventRegister);
         }
 
         [Test]
@@ -22,16 +29,13 @@ namespace GameCore.UnitTests
             CurrentStateShouldBe(ScoreBallState.InCountDown);
         }
 
-        private void CurrentStateShouldBe(ScoreBallState expectedState)
+        [Test]
+        //設定初始化時, 監聽事件
+        public void init_and_register_event()
         {
-            ScoreBallState currentState = scoreBall.CurrentState;
-            Assert.AreEqual(expectedState, currentState);
-        }
+            scoreBall.Init(20);
 
-        private void CurrentCountDownValueShouldBe(int expectedCurrentCountDownValue)
-        {
-            int currentCountDownValue = scoreBall.CurrentCountDownValue;
-            Assert.AreEqual(expectedCurrentCountDownValue, currentCountDownValue);
+            ShouldRegisterBeatEvent();
         }
 
         [Test]
@@ -46,8 +50,54 @@ namespace GameCore.UnitTests
             CurrentCountDownValueShouldBe(0);
             CurrentStateShouldBe(ScoreBallState.None);
         }
-        
+
+        [Test]
         //收到Beat事件時, 倒數數字減一
+        public void count_down_when_receive_beat_event()
+        {
+            scoreBall.Init(20);
+
+            CallBeatEventCallback();
+
+            CurrentCountDownValueShouldBe(19);
+        }
+
+        private void InitEventHandlerMock()
+        {
+            beatEventCallback = null;
+
+            eventRegister = Substitute.For<IEventRegister>();
+
+            eventRegister.When(x => x.Register(Arg.Any<Action<BeatEvent>>())).Do(x =>
+            {
+                Action<BeatEvent> callback = (Action<BeatEvent>)x.Args()[0];
+                beatEventCallback = callback;
+            });
+        }
+
+        private void CallBeatEventCallback()
+        {
+            beatEventCallback.Invoke(new BeatEvent());
+        }
+
+        private void ShouldRegisterBeatEvent()
+        {
+            eventRegister.Received().Unregister(Arg.Any<Action<BeatEvent>>());
+            eventRegister.Received().Register(Arg.Any<Action<BeatEvent>>());
+        }
+
+        private void CurrentStateShouldBe(ScoreBallState expectedState)
+        {
+            ScoreBallState currentState = scoreBall.CurrentState;
+            Assert.AreEqual(expectedState, currentState);
+        }
+
+        private void CurrentCountDownValueShouldBe(int expectedCurrentCountDownValue)
+        {
+            int currentCountDownValue = scoreBall.CurrentCountDownValue;
+            Assert.AreEqual(expectedCurrentCountDownValue, currentCountDownValue);
+        }
+
         //收到Beat事件時, 倒數數字減至0, 發送Damage事件並切換狀態為"Hide"
         //拖曳時, 凍結倒數數字並切換狀態"Freeze"
         //成功結算, 切換狀態為"Hide"
