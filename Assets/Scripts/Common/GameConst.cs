231f3120321925a52930ce606b28ff11f7b72d86
NK.Shien_Home <shien091091@gmail.com>
2024-10-06 22:47:29 +0800

    1.建立場景轉移初始化流程

--- Assets/Scripts/Common/GameConst.cs
+++ Assets/Scripts/Common/GameConst.cs
@@ -4,5 +4,6 @@ namespace GameCore
     {
         public const string DEBUGGER_KEY_SCORE_BALL_VIEW = "ScoreBallView";
         public const string PREFAB_NAME_SCORE_BALL = "ScoreBall";
+        public const string SCENE_REPOSITION_ACTION_ENTER_GAME = "EnterGame";
     }
 }
