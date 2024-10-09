fcbfe47a53f9383078b0f9e09b56db7aa009d068
NK.Shien <lithoshu@yile.com.tw>
2024-10-09 12:13:36 +0800

    1.Installer串接ModelSetting  2.調整ModelSetting  3.更新sub module

--- Assets/Scripts/Installer/ExternalSettingInstaller.cs
+++ Assets/Scripts/Installer/ExternalSettingInstaller.cs
@@ -11,12 +11,14 @@ namespace GameCore
         [SerializeField] private SceneProcessScriptableObject sceneProcessSetting;
         [SerializeField] private GameSettingScriptableObject gameSetting;
         [SerializeField] private ViewPrefabScriptableObject viewPrefabSetting;
+        [SerializeField] private ArchitectureModelSettingScriptableObject modelSetting;
 
         public override void InstallBindings()
         {
             Container.Bind<ISceneProcessSetting>().FromInstance(sceneProcessSetting);
             Container.Bind<IGameSetting>().FromInstance(gameSetting);
             Container.Bind<IViewPrefabSetting>().FromInstance(viewPrefabSetting);
+            Container.Bind<IArchitectureModelSetting>().FromInstance(modelSetting);
         }
     }
 }
