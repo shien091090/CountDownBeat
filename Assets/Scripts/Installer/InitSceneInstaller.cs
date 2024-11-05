4d09fc24d4c77458ab7ea67cad5781d2c866cdf1
NK.Shien <lithoshu@yile.com.tw>
2024-11-05 12:21:41 +0800

    1.InitScene新增SceneProcessManager的引用  2.調整model setting  3.更新sub module

--- Assets/Scripts/Installer/InitSceneInstaller.cs
+++ Assets/Scripts/Installer/InitSceneInstaller.cs
@@ -1,17 +1,21 @@
 using SNShien.Common.AdapterTools;
 using SNShien.Common.AudioTools;
 using SNShien.Common.ProcessTools;
+using UnityEngine;
 using Zenject;
 
 namespace GameCore
 {
     public class InitSceneInstaller : MonoInstaller
     {
+        [SerializeField] private SceneProcessManager sceneProcessManager;
+
         public override void InstallBindings()
         {
             Container.Bind<IAudioManager>().To<FmodAudioManager>().AsSingle();
             Container.Bind<IDeltaTimeGetter>().FromInstance(new DeltaTimeGetter()).AsSingle();
             Container.Bind(typeof(IEventInvoker), typeof(IEventRegister)).To<ArchitectureEventHandler>().AsSingle();
+            Container.Bind<ISceneProcessManager>().FromInstance(sceneProcessManager).AsSingle();
         }
     }
 }
