09722f97c4dd83c34f36a16735ed64288025ee2a
NK.Shien_Home <shien091091@gmail.com>
2024-12-05 23:31:57 +0800

    分數球  1.Add Test Case * 6

--- Assets/Scripts/UnitTests/Editor/ScoreBallTest.cs
+++ Assets/Scripts/UnitTests/Editor/ScoreBallTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
 using SNShien.Common.ProcessTools;
@@ -13,6 +14,10 @@ namespace GameCore.UnitTests
         private IScoreBallPresenter presenter;
 
         private Action<BeatEvent> beatEventCallback;
+        private Action scoreBallBeatEventCallback;
+        private Action<ScoreBallState> updateStateEventCallback;
+        private Action initEventCallback;
+        private Action<int> updateCountDownValueEventCallback;
 
         [SetUp]
         public void Setup()
@@ -23,6 +28,18 @@ namespace GameCore.UnitTests
 
             presenter = Substitute.For<IScoreBallPresenter>();
             scoreBall.BindPresenter(presenter);
+
+            scoreBallBeatEventCallback = Substitute.For<Action>();
+            scoreBall.OnScoreBallBeat += scoreBallBeatEventCallback;
+
+            updateStateEventCallback = Substitute.For<Action<ScoreBallState>>();
+            scoreBall.OnUpdateState += updateStateEventCallback;
+
+            initEventCallback = Substitute.For<Action>();
+            scoreBall.OnInit += initEventCallback;
+
+            updateCountDownValueEventCallback = Substitute.For<Action<int>>();
+            scoreBall.OnUpdateCountDownValue += updateCountDownValueEventCallback;
         }
 
         [Test]
@@ -42,6 +59,15 @@ namespace GameCore.UnitTests
             scoreBall.Init(20);
 
             ShouldRegisterBeatEvent();
+        }
+
+        [Test]
+        //初始化時, 發送初始化事件
+        public void send_init_event_when_init()
+        {
+            scoreBall.Init(20);
+
+            ShouldSendInitEvent(1);
         }
 
         [Test]
@@ -93,60 +119,76 @@ namespace GameCore.UnitTests
         }
 
         [Test]
-        //分數球生成時, 通知Presenter撥放特效
-        public void play_beat_effect_when_spawn()
+        //更新倒數數字時, 發送倒數數字更新事件
+        public void send_update_count_down_value_event_when_update_count_down_value()
         {
             scoreBall.Init(20);
+            
+            ShouldSendUpdateCountDownValueEvent(1);
 
-            ShouldPresenterPlayBeatEffect(1);
+            CallBeatEventCallback(true);
+
+            ShouldSendUpdateCountDownValueEvent(2);
+
+            CallBeatEventCallback(false);
+
+            ShouldSendUpdateCountDownValueEvent(2);
+
+            CallBeatEventCallback(true);
+
+            ShouldSendUpdateCountDownValueEvent(3);
         }
 
         [Test]
-        //分數球重新激活時, 通知Presenter撥放特效
-        public void play_beat_effect_when_reactivate()
+        //分數球重新激活時, 發送Init事件
+        public void send_score_ball_beat_event_when_reactivate()
         {
             scoreBall.Init(20);
+            
+            ShouldSendInitEvent(1);
+            
             scoreBall.SuccessSettle();
-
-            ShouldPresenterPlayBeatEffect(1);
-
+            
+            ShouldSendInitEvent(1);
+            
             scoreBall.Reactivate();
 
-            ShouldPresenterPlayBeatEffect(2);
+            ShouldSendInitEvent(2);
         }
 
         [Test]
-        //收到Beat事件時, 通知Presenter撥放特效
-        public void play_beat_effect_when_receive_beat_event()
+        //收到Beat事件時, 發送分數球Beat事件
+        public void send_score_ball_beat_event_when_receive_beat_event()
         {
             scoreBall.Init(20);
 
-            ShouldPresenterPlayBeatEffect(1);
+            ShouldSendScoreBallBeatEvent(0);
 
             CallBeatEventCallback();
 
-            ShouldPresenterPlayBeatEffect(2);
+            ShouldSendScoreBallBeatEvent(1);
 
             CallBeatEventCallback();
 
-            ShouldPresenterPlayBeatEffect(3);
+            ShouldSendScoreBallBeatEvent(2);
         }
 
         [Test]
-        //收到Beat事件時, 若狀態為"Hide"則不會通知Presenter撥放特效
-        public void do_not_play_beat_effect_when_hide()
+        //收到Beat事件時, 若狀態為"Hide"則不會發送分數球Beat事件
+        public void do_not_send_score_ball_beat_event_when_hide()
         {
             scoreBall.Init(20);
 
-            ShouldPresenterPlayBeatEffect(1);
+            CallBeatEventCallback();
+            
+            ShouldSendScoreBallBeatEvent(1);
 
             scoreBall.SuccessSettle();
-
             CurrentStateShouldBe(ScoreBallState.Hide);
 
             CallBeatEventCallback();
 
-            ShouldPresenterPlayBeatEffect(1);
+            ShouldSendScoreBallBeatEvent(1);
         }
 
         [Test]
@@ -163,16 +205,6 @@ namespace GameCore.UnitTests
 
             CurrentCountDownValueShouldBe(20);
             CurrentStateShouldBe(ScoreBallState.Freeze);
-        }
-
-        [Test]
-        //拖曳時, 通知Presenter更新狀態(inCountDown -> Freeze)
-        public void update_state_when_set_freeze()
-        {
-            scoreBall.Init(20);
-            scoreBall.SetFreezeState(true);
-
-            ShouldPresenterUpdateState(1, ScoreBallState.Freeze);
         }
 
         [Test]
@@ -203,6 +235,23 @@ namespace GameCore.UnitTests
             CallBeatEventCallback();
 
             CurrentCountDownValueShouldBe(9);
+        }
+
+        [Test]
+        //變更狀態時, 會發送變更狀態事件
+        public void send_update_state_event_when_state_change()
+        {
+            scoreBall.Init(10);
+            LastSendUpdateStateEventShouldBe(ScoreBallState.InCountDown);
+
+            scoreBall.SetFreezeState(true);
+            LastSendUpdateStateEventShouldBe(ScoreBallState.Freeze);
+
+            scoreBall.SetFreezeState(false);
+            LastSendUpdateStateEventShouldBe(ScoreBallState.InCountDown);
+
+            scoreBall.SuccessSettle();
+            LastSendUpdateStateEventShouldBe(ScoreBallState.Hide);
         }
 
         [Test]
@@ -311,17 +360,31 @@ namespace GameCore.UnitTests
             beatEventCallback?.Invoke(new BeatEvent(isCountDownBeat));
         }
 
-        private void ShouldPresenterPlayBeatEffect(int expectedCallTimes)
+        private void LastSendUpdateStateEventShouldBe(ScoreBallState expectedState)
         {
-            // if (expectedCallTimes == 0)
-            //     presenter.DidNotReceive().PlayBeatEffect();
-            // else
-            //     presenter.Received(expectedCallTimes).PlayBeatEffect();
+            ScoreBallState arg = (ScoreBallState)updateStateEventCallback.ReceivedCalls().Last().GetArguments()[0];
+            Assert.AreEqual(expectedState, arg);
         }
 
-        private void ShouldPresenterUpdateState(int expectedCallTimes, ScoreBallState expectedNewState)
+        private void ShouldSendInitEvent(int expectedCallTimes)
         {
-            // presenter.Received(expectedCallTimes).UpdateState(expectedNewState);
+            if (expectedCallTimes == 0)
+                initEventCallback.DidNotReceive().Invoke();
+            else
+                initEventCallback.Received(expectedCallTimes).Invoke();
+        }
+
+        private void ShouldSendScoreBallBeatEvent(int expectedCallTimes)
+        {
+            if (expectedCallTimes == 0)
+                scoreBallBeatEventCallback.DidNotReceive().Invoke();
+            else
+                scoreBallBeatEventCallback.Received(expectedCallTimes).Invoke();
+        }
+
+        private void ShouldSendUpdateStateEvent(int expectedCallTimes, ScoreBallState expectedNewState)
+        {
+            updateStateEventCallback.Received(expectedCallTimes).Invoke(expectedNewState);
         }
 
         private void ShouldSendDamageEvent(int expectedCallTimes = 1)
@@ -345,6 +408,14 @@ namespace GameCore.UnitTests
         {
             int currentCountDownValue = scoreBall.CurrentCountDownValue;
             Assert.AreEqual(expectedCurrentCountDownValue, currentCountDownValue);
+        }
+
+        private void ShouldSendUpdateCountDownValueEvent(int expectedCallTimes)
+        {
+            if (expectedCallTimes == 0)
+                updateCountDownValueEventCallback.DidNotReceive().Invoke(Arg.Any<int>());
+            else
+                updateCountDownValueEventCallback.Received(expectedCallTimes).Invoke(Arg.Any<int>());
         }
     }
 }
