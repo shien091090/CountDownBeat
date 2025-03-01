cbdb31d2f4fbf01002052696c75afee138f040dd
NKShien_Home <shien091091@gmail.com>
2025-03-01 11:19:18 +0800

    分數球旗標設定  1.拿掉用不到的Attribute

--- Assets/Scripts/ScoreBall/ScoreBallFrameColorByFlagScriptableObject.cs
+++ Assets/Scripts/ScoreBall/ScoreBallFrameColorByFlagScriptableObject.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace GameCore
 {
-    [CreateAssetMenu]
     public class ScoreBallFrameColorByFlagScriptableObject : SerializedScriptableObject, IScoreBallFrameColorByFlagSetting
     {
         [SerializeField] private List<ScoreBallFrameColorDefine> frameColorDefines;
